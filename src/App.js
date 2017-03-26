a2ff32e81cb49aa0dc7e95010b4afe75a904fcfb
Enol Iglesias <enoliglesias@gmail.com>
2017-03-26 23:58:03 +0200

    Render RecipeList component from App with all recipes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import RecipeList from './RecipeList';
 import './App.css';
 
 class App extends Component {
@@ -23,13 +23,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Recipe List</h1>
+        <RecipeList recipes={this.state.recipes} />
       </div>
     );
   }
